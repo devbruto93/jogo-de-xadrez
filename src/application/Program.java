b368424cb441455a7eecc1d71a51321193803b5c
devbruto93 <douglas.modest.2013@gmail.com>
2025-11-27 08:33:05 -0300

    criaçao das classes Board, Piece e Position.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-
-        System.out.println("Testanto a classe!");
+        Board board = new Board(8,8);
     }
 }
