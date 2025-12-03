dbdb35eb851ac1f114b376606d61eb7379b1ab62
devbruto93 <douglas.modest.2013@gmail.com>
2025-12-03 08:12:22 -0300

    limpando a tela e tratando excessoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,11 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.CheesMatch;
+import chess.ChessException;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -15,16 +17,28 @@ public class Program {
         CheesMatch cheesMatch = new CheesMatch();
 
         while (true) {
-            UI.printBoard(cheesMatch.getPieces());
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try {
 
-            System.out.println();
-            System.out.println("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = cheesMatch.performChessMove(source, target);
+                UI.clearScreen();
+                UI.printBoard(cheesMatch.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = cheesMatch.performChessMove(source, target);
+                 } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+                 } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
+
